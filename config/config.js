62f225ee73aed91f26850a65238d351ec8fce419
PedroD <pedelon@gmail.com>
2020-06-19 20:41:04 -0400

    rest server - usuario

--- config/config.js
+++ config/config.js
@@ -3,3 +3,23 @@
 // ======================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ======================
+//       entorno
+// ======================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ======================
+//    Base de Datos
+// ======================
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://pedelon:ZZBL5M0A5hCxWZTu@cluster0-ojwkd.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
